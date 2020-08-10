54510eccc7f8636acd8be016654b9970e3361711
Lei Yang <ehp246@yahoo.com>
2020-08-10 09:35:03 -0400

    A wrong merge.

--- src/main/java/me/ehp246/aufrest/api/configuration/ByRestConfiguration.java
+++ src/main/java/me/ehp246/aufrest/api/configuration/ByRestConfiguration.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.module.mrbean.MrBeanModule;
 
 import me.ehp246.aufrest.api.rest.AuthenticationProvider;
 import me.ehp246.aufrest.api.rest.ClientConfig;
+import me.ehp246.aufrest.api.rest.HttpUtils;
 import me.ehp246.aufrest.api.rest.MediaType;
 import me.ehp246.aufrest.api.rest.TextContentConsumer;
 import me.ehp246.aufrest.api.rest.TextContentProducer;
@@ -33,30 +34,6 @@ public class ByRestConfiguration {
 	public JdkClientProvider jdkClientProvider(final ClientConfig clientConfig,
 			@Autowired(required = false) final AuthenticationProvider authProvider) {
 		return new JdkClientProvider(clientConfig, authProvider);
-	}
-
-	@Bean
-	public JsonByJackson jsonByJackson(@Autowired(required = false) final ObjectMapper objectMapper) {
-		return new JsonByJackson(Optional.ofNullable(objectMapper).orElseGet(() -> {
-			final var newMapper = new ObjectMapper().setSerializationInclusion(Include.NON_NULL)
-					.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-					.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).registerModule(new JavaTimeModule())
-					.registerModule(new MrBeanModule());
-
-			var module = InvocationUtil.invokeWithDefault(
-					() -> Class.forName("com.fasterxml.jackson.datatype.jsr310.JavaTimeModule.JavaTimeModule")
-							.getDeclaredConstructor((Class<?>[]) null).newInstance(),
-					null);
-
-			Optional.ofNullable(module).map(m -> newMapper.registerModule((com.fasterxml.jackson.databind.Module) m));
-
-			module = InvocationUtil
-					.invokeWithDefault(() -> Class.forName("com.fasterxml.jackson.module.mrbean.MrBeanModule")
-							.getDeclaredConstructor((Class<?>[]) null).newInstance(), null);
-
-			Optional.ofNullable(module).map(m -> newMapper.registerModule((com.fasterxml.jackson.databind.Module) m));
-			return objectMapper;
-		}));
 	}
 
 	@Bean
@@ -102,11 +79,15 @@ public class ByRestConfiguration {
 			@Override
 			public TextContentProducer contentProducer(String mediaType) {
 				mediaType = mediaType.toLowerCase();
-				if (mediaType.startsWith(MediaType.APPLICATION_JSON)) {
+				if (mediaType.startsWith(HttpUtils.APPLICATION_JSON)) {
 					return jackson::toText;
 				}
 
-				return Object::toString;
+				if (mediaType.startsWith(HttpUtils.TEXT_PLAIN)) {
+					return Object::toString;
+				}
+
+				throw new RuntimeException("Un-supported media type: " + mediaType);
 			}
 
 			@Override
@@ -115,27 +96,10 @@ public class ByRestConfiguration {
 				if (mediaType.startsWith(MediaType.APPLICATION_JSON)) {
 					return jackson::fromText;
 				}
-				return (text, receiver) -> text;
-			}
-
-		};
-	}
-
-	@Bean
-	public ClientConfig clientConfig(
-			@Value("${" + AufRestConstants.CONNECT_TIMEOUT + ":" + AufRestConstants.CONNECT_TIMEOUT_DEFAULT
-					+ "}") final long connectTimeout,
-			@Value("${" + AufRestConstants.RESPONSE_TIMEOUT + ":" + AufRestConstants.RESPONSE_TIMEOUT_DEFAULT
-					+ "}") final long requestTimeout) {
-		return new ClientConfig() {
-			@Override
-			public Duration connectTimeout() {
-				return Duration.ofMillis(connectTimeout);
-			}
-
-			@Override
-			public Duration responseTimeout() {
-				return Duration.ofMillis(requestTimeout);
+				if (mediaType.startsWith(HttpUtils.TEXT_PLAIN)) {
+					return (text, receiver) -> text;
+				}
+				throw new RuntimeException("Un-supported media type: " + mediaType);
 			}
 
 		};
