ac410c0b908892b9d522dc5532a4100b975f5be6
Lei Yang <ehp246@yahoo.com>
2020-10-10 16:04:39 -0400

    Fixed the href.

--- src/main/java/me/ehp246/aufrest/api/configuration/AufRestConstants.java
+++ src/main/java/me/ehp246/aufrest/api/configuration/AufRestConstants.java
@@ -12,24 +12,26 @@ public class AufRestConstants {
 	 * The property name to configure the connection timeout duration for HttpClient
 	 * globally.
 	 * <p>
-	 * The value should follow ISO 8601 Duration standard. See
-	 * {@link https://en.wikipedia.org/wiki/ISO_8601#Durations}.
+	 * The value should follow ISO 8601 Duration standard.
 	 * <p>
 	 * Not set by default. I.e., it never times out.
 	 *
 	 * @see java.net.http.HttpClient.Builder
+	 * @see <a href= "https://en.wikipedia.org/wiki/ISO_8601#Durations">ISO_8601
+	 *      Durations</a>
 	 */
 	public static final String CONNECT_TIMEOUT = "me.ehp246.aufrest.connectTimeout";
 	/**
 	 * The property name to configure the timeout duration waiting for a response
 	 * for a request.
 	 * <p>
-	 * The value should follow ISO 8601 Duration standard. See
-	 * {@link https://en.wikipedia.org/wiki/ISO_8601#Durations}.
+	 * The value should follow ISO 8601 Duration standard.
 	 * <p>
 	 * Not set by default. I.e., it never times out.
 	 *
 	 * @see java.net.http.HttpRequest.Builder
+	 * @see <a href= "https://en.wikipedia.org/wiki/ISO_8601#Durations">ISO_8601
+	 *      Durations</a>
 	 */
 	public static final String RESPONSE_TIMEOUT = "me.ehp246.aufrest.responseTimeout";
 
