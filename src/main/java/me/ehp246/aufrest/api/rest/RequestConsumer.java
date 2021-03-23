7a9d2077aadae8d9d172d7297d99cd0fc4407146
Lei Yang <ehp246@yahoo.com>
2021-03-23 11:16:19 -0400

    Testing Consumers.

--- src/main/java/me/ehp246/aufrest/api/rest/RequestConsumer.java
+++ src/main/java/me/ehp246/aufrest/api/rest/RequestConsumer.java
@@ -2,7 +2,13 @@ package me.ehp246.aufrest.api.rest;
 
 import java.net.http.HttpRequest;
 
+import org.springframework.core.annotation.Order;
+
 /**
+ * Defines a Spring bean that AufRest invokes before a HTTP request is sent.
+ * <p>
+ * Multiple beans are supported and invoked by the {@link Order}.
+ * 
  * @author Lei Yang
  * @since 2.2.2
  */
--- src/test/java/me/ehp246/aufrest/integration/local/filter/AppConfig.java
+++ src/test/java/me/ehp246/aufrest/integration/local/filter/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 
 import me.ehp246.aufrest.api.annotation.EnableByRest;
 import me.ehp246.aufrest.api.rest.RequestFilter;
@@ -20,12 +21,24 @@ import me.ehp246.aufrest.mock.Jackson;
 @Import({ Jackson.class, RequestLogger.class })
 class AppConfig {
 	@Bean
-	public RequestFilter requestFilter(final ReqFilter reqFilter) {
+	RequestFilter requestFilter(final ReqFilter reqFilter) {
 		return reqFilter::apply;
 	}
 	
 	@Bean
-	public ResponseFilter responseFilter(final RespFilter respFilter) {
+	ResponseFilter responseFilter(final RespFilter respFilter) {
 		return respFilter::apply;
+	}
+
+	@Bean
+	@Order(2)
+	ReqConsumer reqConsumer01() {
+		return new ReqConsumer(2);
+	}
+
+	@Bean
+	@Order(1)
+	ReqConsumer reqConsumer02() {
+		return new ReqConsumer(1);
 	}
 }
