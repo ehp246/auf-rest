d3bd6fe14c4b2dda1595be5994b2b1f16008c0bd
Lei Yang <ehp246@yahoo.com>
2022-06-16 16:44:43 -0400

    ReturnType in progress.

--- src/test/java/me/ehp246/aufrest/core/byrest/returntype/ReturnTypeTest.java
+++ src/test/java/me/ehp246/aufrest/core/byrest/returntype/ReturnTypeTest.java
@@ -1,17 +1,19 @@
 package me.ehp246.aufrest.core.byrest.returntype;
 
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.mock.env.MockEnvironment;
 
+import me.ehp246.aufrest.api.rest.RestClientConfig;
 import me.ehp246.aufrest.api.rest.RestFn;
 import me.ehp246.aufrest.api.rest.RestRequest;
 import me.ehp246.aufrest.core.byrest.ByRestProxyFactory;
+import me.ehp246.aufrest.core.byrest.DefaultProxyMethodParser;
 
 /**
  * @author Lei Yang
@@ -19,12 +21,13 @@ import me.ehp246.aufrest.core.byrest.ByRestProxyFactory;
  */
 class ReturnTypeTest {
     private final AtomicReference<RestRequest> reqRef = new AtomicReference<>();
-    private final RestFn client = request -> {
+    private final RestFn restFn = request -> {
         reqRef.set(request);
         return Mockito.mock(HttpResponse.class);
     };
-    private final ByRestProxyFactory factory = new ByRestProxyFactory(cfg -> client,
-            new MockEnvironment()::resolveRequiredPlaceholders);
+    private final ByRestProxyFactory factory = new ByRestProxyFactory(cfg -> restFn, new RestClientConfig(),
+            Object::toString, new DefaultProxyMethodParser(Object::toString, name -> null,
+                    name -> BodyHandlers.discarding(), binding -> BodyHandlers.discarding()));
 
     @BeforeEach
     void beforeEach() {
@@ -33,14 +36,12 @@ class ReturnTypeTest {
 
     @Test
     void return_type_001() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                factory.newInstance(ReturnTypeCase001.class)::get001);
+        Assertions.assertThrows(IllegalArgumentException.class, factory.newInstance(ReturnTypeCase001.class)::get001);
     }
 
     @Test
     void return_type_002() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                factory.newInstance(ReturnTypeCase001.class)::get002);
+        Assertions.assertThrows(IllegalArgumentException.class, factory.newInstance(ReturnTypeCase001.class)::get002);
     }
 
     @Test
