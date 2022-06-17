401e2ec38c5bb2ad521566048e80547a48cdc480
Lei Yang <ehp246@yahoo.com>
2022-06-17 12:49:17 -0400

    Use the simple newInstance on the factory.

--- src/main/java/me/ehp246/aufrest/core/byrest/ByRestRegistrar.java
+++ src/main/java/me/ehp246/aufrest/core/byrest/ByRestRegistrar.java
@@ -1,6 +1,5 @@
 package me.ehp246.aufrest.core.byrest;
 
-import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
@@ -13,11 +12,7 @@ import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
 
 import me.ehp246.aufrest.api.annotation.ByRest;
-import me.ehp246.aufrest.api.annotation.Default;
 import me.ehp246.aufrest.api.annotation.EnableByRest;
-import me.ehp246.aufrest.api.rest.AuthScheme;
-import me.ehp246.aufrest.api.rest.ByRestProxyConfig;
-import me.ehp246.aufrest.api.rest.ByRestProxyConfig.AuthConfig;
 
 public final class ByRestRegistrar implements ImportBeanDefinitionRegistrar {
     private final static Logger LOGGER = LogManager.getLogger(ByRestRegistrar.class);
@@ -39,24 +34,16 @@ public final class ByRestRegistrar implements ImportBeanDefinitionRegistrar {
 
             final var name = byRestInterface.getAnnotation(ByRest.class).name();
 
-            registry.registerBeanDefinition(name.equals("") ? byRestInterface.getName() : name,
+            registry.registerBeanDefinition(name.isBlank() ? byRestInterface.getName() : name,
                     this.getProxyBeanDefinition(metadata.getAnnotationAttributes(EnableByRest.class.getCanonicalName()),
                             byRestInterface));
         });
     }
 
     private BeanDefinition getProxyBeanDefinition(Map<String, Object> map, final Class<?> byRestInterface) {
-        final var byRest = byRestInterface.getAnnotation(ByRest.class);
-        final var globalErrorType = (Class<?>) map.get("errorType");
-
         final var args = new ConstructorArgumentValues();
 
         args.addGenericArgumentValue(byRestInterface);
-        args.addGenericArgumentValue(new ByRestProxyConfig(byRest.value(),
-                new AuthConfig(Arrays.asList(byRest.auth().value()), AuthScheme.valueOf(byRest.auth().scheme().name())),
-                byRest.timeout(), byRest.accept(), byRest.contentType(), byRest.acceptGZip(),
-                byRest.errorType() == Default.class ? globalErrorType : byRest.errorType(),
-                byRest.responseBodyHandler()));
 
         final var beanDef = new GenericBeanDefinition();
         beanDef.setBeanClass(byRestInterface);
