85fa96d2ca747b7538263bcc498ac27b3b4334d9
Lei Yang <ehp246@yahoo.com>
2023-01-18 16:48:54 -0500

    Removed the bean definition. (#138)

--- src/test/java/me/ehp246/test/app/objectmapper/ObjectMapperTest.java
+++ src/test/java/me/ehp246/test/app/objectmapper/ObjectMapperTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import me.ehp246.aufrest.api.configuration.ObjectMapperConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import me.ehp246.aufrest.provider.jackson.JsonByObjectMapper;
 import me.ehp246.test.mock.Jackson;
 
 /**
@@ -29,7 +31,7 @@ class ObjectMapperTest {
         appCtx.register(AppConfig.Config01.class);
         appCtx.refresh();
 
-        Assertions.assertEquals(Jackson.OBJECT_MAPPER, appCtx.getBean("aufRestObjectMapper"));
+        Assertions.assertEquals(true, appCtx.getBean(JsonByObjectMapper.class) != null);
     }
 
     @Test
@@ -38,7 +40,7 @@ class ObjectMapperTest {
         appCtx.register(AppConfig.Config04.class);
         appCtx.refresh();
 
-        Assertions.assertEquals(Jackson.OBJECT_MAPPER, appCtx.getBean("aufRestObjectMapper"));
+        Assertions.assertEquals(true, appCtx.getBean(JsonByObjectMapper.class) != null);
     }
 
     @Test
@@ -47,7 +49,7 @@ class ObjectMapperTest {
         appCtx.register(AppConfig.Config02.class);
         appCtx.refresh();
 
-        Assertions.assertEquals(Jackson.OBJECT_MAPPER, appCtx.getBean("aufRestObjectMapper"));
+        Assertions.assertEquals(Jackson.OBJECT_MAPPER, appCtx.getBean(ObjectMapper.class));
     }
 
     @Test
@@ -56,8 +58,6 @@ class ObjectMapperTest {
         appCtx.register(AppConfig.Config03.class);
         appCtx.refresh();
 
-        Assertions.assertEquals(true, appCtx.getBean("aufRestObjectMapper") != null);
-        Assertions.assertEquals(ObjectMapperConfiguration.class.getName(),
-                appCtx.getBeanDefinition("aufRestObjectMapper").getFactoryBeanName());
+        Assertions.assertEquals(true, appCtx.getBean(JsonByObjectMapper.class) != null);
     }
 }
